6aa02e62c60d2dddf3631d128ae19ebf034d715a
nptayusherly <nptayusherly@gmail.com>
2020-05-08 09:49:07 +0800

    update db mahasiswa.sql

--- SortingFiltering/mahasiswa.sql
+++ SortingFiltering/mahasiswa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 06 Bulan Mei 2020 pada 17.53
+-- Waktu pembuatan: 08 Bulan Mei 2020 pada 03.47
 -- Versi server: 10.1.40-MariaDB
 -- Versi PHP: 7.3.5
 
@@ -34,28 +34,28 @@ CREATE TABLE `tbmahasiswa` (
   `nama_mahasiswa` varchar(255) NOT NULL,
   `alamat` text NOT NULL,
   `jenis_kelamin` varchar(255) NOT NULL,
-  `jurusan` varchar(255) NOT NULL
+  `fakultas` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
 --
 -- Dumping data untuk tabel `tbmahasiswa`
 --
 
-INSERT INTO `tbmahasiswa` (`id`, `nim`, `nama_mahasiswa`, `alamat`, `jenis_kelamin`, `jurusan`) VALUES
-(1, '1808561001', 'SAFIRA\r\n', 'Cilacap', 'Perempuan', 'Sistem Informasi'),
-(2, '1808561002', 'NI PUTU AMBALIKA DEWI\r\n', 'Jakarta', 'Perempuan', 'Teknik Informatika'),
-(3, '1808561003', 'AYU KADEK NADYA OKTAVIANA\r\n', 'Yogyakarta', 'Perempuan', 'Sistem Informasi'),
-(4, '1808561004', 'GUSTI AYU VIDJARETHA WARDANA\r\n', 'Semarang', 'Perempuan', 'Teknik Informatika'),
-(5, '1808561005', 'NI KADEK AYU TASYA OKTA VIANA\r\n', 'Purwokerto', 'Perempuan', 'Sistem Informasi'),
-(6, '1808561006', 'LUH PUTU EKA NADYA WATI\r\n', 'Njojog', 'Perempuan', 'Sistem Informasi'),
-(7, '1808561007', 'HAIRUL LANA\r\n', 'Prapatan Ciamis', 'Laki-laki', 'Sistem Informasi'),
-(8, '1808561008', 'FARIN ISTIGHFARIZKY\r\n', 'Jakarta', 'Perempuan', 'Sistem Informasi'),
-(9, '1808561010', 'I MADE ARI WIDIARSANA\r\n', 'Jakarta', 'Laki-laki', 'Teknik Informatika'),
-(10, '1808561011', 'MUHAMMAD FIRDAUS ZULKARNAIN\r\n', 'Jakarta', 'Laki-laki', 'Sistem Informasi'),
-(11, '1808561012', 'SANG PUTU FEBRI WIRA PRATAMA\r\n', 'Medan', 'Laki-laki', 'Sistem Informasi'),
-(12, '1808561013', 'I MADE SATRIA BIMANTARA\r\n', 'Palangkaraya', 'Laki-laki', 'Teknik Informatika'),
-(13, '1808561014', 'KARLINA SURYA WITANTO\r\n', 'Riau', 'Perempuan', 'Sistem Informasi'),
-(14, '1808561015', 'NI MADE PUTRI WAHYUNI\r\n', 'Jakarta', 'Perempuan', 'Teknik Informatika');
+INSERT INTO `tbmahasiswa` (`id`, `nim`, `nama_mahasiswa`, `alamat`, `jenis_kelamin`, `fakultas`) VALUES
+(1, '1808561001', 'SAFIRA\r\n', 'Cilacap', 'Perempuan', 'MIPA'),
+(2, '1808561002', 'NI PUTU AMBALIKA DEWI\r\n', 'Jakarta', 'Perempuan', 'MIPA'),
+(3, '1808561003', 'AYU KADEK NADYA OKTAVIANA\r\n', 'Yogyakarta', 'Perempuan', 'TEKNIK'),
+(4, '1808561004', 'GUSTI AYU VIDJARETHA WARDANA\r\n', 'Semarang', 'Perempuan', 'MIPA'),
+(5, '1808561005', 'NI KADEK AYU TASYA OKTA VIANA\r\n', 'Purwokerto', 'Perempuan', 'TEKNIK'),
+(6, '1808561006', 'LUH PUTU EKA NADYA WATI\r\n', 'Njojog', 'Perempuan', 'TEKNIK'),
+(7, '1808561007', 'HAIRUL LANA\r\n', 'Prapatan Ciamis', 'Laki-laki', 'TEKNIK'),
+(8, '1808561008', 'FARIN ISTIGHFARIZKY\r\n', 'Jakarta', 'Perempuan', 'MIPA'),
+(9, '1808561010', 'I MADE ARI WIDIARSANA\r\n', 'Jakarta', 'Laki-laki', 'TEKNIK'),
+(10, '1808561011', 'MUHAMMAD FIRDAUS ZULKARNAIN\r\n', 'Jakarta', 'Laki-laki', 'MIPA'),
+(11, '1808561012', 'SANG PUTU FEBRI WIRA PRATAMA\r\n', 'Medan', 'Laki-laki', 'TEKNIK'),
+(12, '1808561013', 'I MADE SATRIA BIMANTARA\r\n', 'Palangkaraya', 'Laki-laki', 'MIPA'),
+(13, '1808561014', 'KARLINA SURYA WITANTO\r\n', 'Riau', 'Perempuan', 'TEKNIK'),
+(14, '1808561015', 'NI MADE PUTRI WAHYUNI\r\n', 'Jakarta', 'Perempuan', 'TEKNIK');
 
 --
 -- Indexes for dumped tables
@@ -75,7 +75,7 @@ ALTER TABLE `tbmahasiswa`
 -- AUTO_INCREMENT untuk tabel `tbmahasiswa`
 --
 ALTER TABLE `tbmahasiswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
